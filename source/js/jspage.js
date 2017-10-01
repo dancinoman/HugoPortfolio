9ada190a7fed1d835264660d20ffb432daf9a48a
Hugo-PC\Hugo <dancinoman@gmail.com>
2017-09-30 22:33:53 -0400

    New portfolio project

---
+++ source/js/jspage.js
@@ -0,0 +1,17 @@
+var options = function(){
+
+	var index = country.length;
+	var selection = "";
+
+	for(i=0; i < index; i++)
+	{
+		 selection +=	"<option>" + country[i] + "</option>";
+	}
+	
+	return selection;
+}
+
+
+
+
+$("#country-selection").prepend( "<select>"+ options() +"</select>");
